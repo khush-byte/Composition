185d037dc6d4694561d07920b7168827e431f224
khush-byte <khush4450@gmail.com>
2022-11-03 16:59:44 +0500

    used binding

--- app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -4,32 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.composition.R
+import com.example.composition.databinding.FragmentChooseLevelBinding
 
 class ChooseLevelFragment : Fragment() {
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw java.lang.RuntimeException("FragmentChooseLevelBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val transaction = requireActivity().supportFragmentManager.beginTransaction()
-        val buttonTestLevel = view.findViewById<Button>(R.id.btn_test_level)
 
-        buttonTestLevel.setOnClickListener{
+        binding.btnTestLevel.setOnClickListener {
             transaction.addToBackStack(null)
             transaction.replace(R.id.fragmentContainerView, GameFragment())
             transaction.commit()
         }
+    }
 
-
-
-
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
@@ -4,30 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.composition.R
+import com.example.composition.databinding.FragmentWelcomeBinding
 
-class WelcomeFragment: Fragment() {
+class WelcomeFragment : Fragment() {
+    //Use the Binding (link)
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        //Set params to binding
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        //Return root as view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val buttonStart = view.findViewById<Button>(R.id.start_btn)
-
-        buttonStart.setOnClickListener {
+        binding.buttonUnderstand.setOnClickListener {
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
             transaction.addToBackStack(null)
             transaction.replace(R.id.fragmentContainerView, ChooseLevelFragment())
             transaction.commit()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
